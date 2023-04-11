dec39674aabf769bbb87c18be338561cf8478212
macbookair2020 <uskanovjahongir1199@gmail.com>
2023-04-11 07:22:11 +0500

    Music app connected with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:audioplayer/provider/audio_provider.dart';
 import 'package:audioplayer/router/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+bool shouldUseFirestoreEmulator = false;
 
-void main() {
+
+
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // if (shouldUseFirestoreEmulator) {
+  //   FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  // }
   runApp(
     MultiProvider(
       providers: [
