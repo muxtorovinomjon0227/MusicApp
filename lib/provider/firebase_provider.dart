3b15e547af7607aef86c9558723f0053aec812e4
macbookair2020 <uskanovjahongir1199@gmail.com>
2023-04-11 09:29:39 +0500

    get from firebase imag

--- lib/provider/firebase_provider.dart
+++ lib/provider/firebase_provider.dart
@@ -4,6 +4,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 class FirebaseProvider extends ChangeNotifier{
 
   Future<void> downloadURLExample() async {
+
    final downloadURL = await FirebaseStorage.instance.ref().child("Image.png").getDownloadURL();
 
   }
